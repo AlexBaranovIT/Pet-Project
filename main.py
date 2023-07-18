d34260a8f42358bc08081b01109d548b31288bbe
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-18 09:27:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from telebot import TeleBot, types
 import os
 
+TOKEN = 'YOUR TELEGRAM BOT TOKEN'
+bot = TeleBot(TOKEN)
+
+
 @bot.message_handler(commands=['start']):
 def start(message):
     welcome_message = 'Hello, ' + message.from_user.first_name + message.from_user.last_name
     bot.send_message(message.chat.id, welcome_message)
-  
+
+
+bot.polling(none_stop=True)
