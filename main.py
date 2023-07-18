e1da5ad84f5d4387499c0cdf217b351cf6855ed1
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-18 09:25:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from telebot import TeleBot, types
 import os
 
-@bot.message_handler(commands=['start]):
+@bot.message_handler(commands=['start']):
+def start(message):
+    welcome_message = 'Hello, ' + message.from_user.first_name + message.from_user.last_name
+    bot.send_message(message.chat.id, welcome_message)
