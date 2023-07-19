3de7cd950d6eb7698667f1d5f5249d185a625fff
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-19 11:14:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 TOKEN = 'YOUR TELEGRAM BOT TOKEN'
 bot = TeleBot(TOKEN)
 
-
+#Function that reacts on command start
 @bot.message_handler(commands=['start']):
 def start(message):
     welcome_message = 'Hello, ' + message.from_user.first_name + message.from_user.last_name
